7185be68168de50bd4e92b1723a6a70b0cf19fce
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2025-05-29 20:48:45 -0600

    Update CorsConfig.java

--- src/main/java/com/rrii/gestionestudiantes/security/CorsConfig.java
+++ src/main/java/com/rrii/gestionestudiantes/security/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 public class CorsConfig {
 
@@ -28,4 +29,3 @@ public class CorsConfig {
         return source;
     }
 }
-
