49398733d4eb6b81e970a9592780cd93519e375e
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2025-06-01 23:18:09 -0600

    Update TfgController.java

--- src/main/java/com/rrii/gestionestudiantes/controller/TfgController.java
+++ src/main/java/com/rrii/gestionestudiantes/controller/TfgController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import com.rrii.gestionestudiantes.model.Tfg;
 import com.rrii.gestionestudiantes.repository.EstudianteRepository;
