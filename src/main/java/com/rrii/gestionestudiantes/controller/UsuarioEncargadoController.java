43e589e029e18c49027d03b50bf06b592297e6ef
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2025-06-03 20:07:04 -0600

    Update UsuarioEncargadoController.java

--- src/main/java/com/rrii/gestionestudiantes/controller/UsuarioEncargadoController.java
+++ src/main/java/com/rrii/gestionestudiantes/controller/UsuarioEncargadoController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.http.HttpStatus;
+
 
 import com.rrii.gestionestudiantes.model.UsuarioEncargado;
 import com.rrii.gestionestudiantes.repository.UsuarioEncargadoRepository;
