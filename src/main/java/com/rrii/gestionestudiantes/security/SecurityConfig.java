8d2b19e559280a8ed653d3eafbbec152c0387f38
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2025-06-03 22:13:43 -0600

    Update SecurityConfig.java

--- src/main/java/com/rrii/gestionestudiantes/security/SecurityConfig.java
+++ src/main/java/com/rrii/gestionestudiantes/security/SecurityConfig.java
@@ -2,29 +2,31 @@ package com.rrii.gestionestudiantes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigSource) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(corsConfigSource))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/auth/**",
-                    "/estudiantes/fotos/**"
+                    "/estudiantes/**",
+                    "/estudiantes/fotos/**",
+                    "/tfgs/**",
+                    "/encargado/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .httpBasic(httpBasic -> httpBasic.disable());
+            .cors(cors -> cors.configurationSource(corsConfigurationSource)) // 👈 ¡Este es el cambio clave!
+            .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
