72dbf0d7419737298b8de64feb919312a4bc3cd2
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2025-06-24 10:05:57 -0600

    Update EstudianteRepository.java

--- src/main/java/com/rrii/gestionestudiantes/repository/EstudianteRepository.java
+++ src/main/java/com/rrii/gestionestudiantes/repository/EstudianteRepository.java
@@ -1,7 +1,18 @@
 package com.rrii.gestionestudiantes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.rrii.gestionestudiantes.model.Estudiante;
 
-public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {}
+/**
+ * Repositorio para la entidad Estudiante.
+ * Extiende JpaRepository para proporcionar operaciones CRUD automáticas.
+ *
+ * - Estudiante: entidad que gestiona.
+ * - Long: tipo del identificador primario (ID).
+ *
+ * Spring Data JPA implementa automáticamente esta interfaz en tiempo de ejecución.
+ */
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+    // Puedes agregar aquí métodos personalizados si lo necesitas, por ejemplo:
+    // List<Estudiante> findByProgramaMaestria(String programa);
+}
