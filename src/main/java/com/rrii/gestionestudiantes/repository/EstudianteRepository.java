b78c098499cb3752412a3b2ba9ae4b6b7c43c5d6
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2025-06-24 11:14:37 -0600

    Update EstudianteRepository.java

--- src/main/java/com/rrii/gestionestudiantes/repository/EstudianteRepository.java
+++ src/main/java/com/rrii/gestionestudiantes/repository/EstudianteRepository.java
@@ -1,7 +1,17 @@
 package com.rrii.gestionestudiantes.repository;
 
+// Importa la interfaz JpaRepository de Spring Data JPA
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Importa la entidad Estudiante que este repositorio manejará
 import com.rrii.gestionestudiantes.model.Estudiante;
 
+/**
+ * Esta interfaz actúa como el repositorio de acceso a datos para la entidad Estudiante.
+ * Extiende JpaRepository, lo que le proporciona métodos CRUD automáticamente (findAll, save, deleteById, etc.)
+ *
+ * Parámetros genéricos:
+ * - Estudiante: la entidad a manejar
+ * - Long: el tipo de la clave primaria de la entidad (en este caso, Long)
+ */
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {}
